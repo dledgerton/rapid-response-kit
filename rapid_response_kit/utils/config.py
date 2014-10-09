2771408ae6b5058e445a0e84b3921b5ed3570e11
dledgerton <davidedgertonjr@gmail.com>
2014-10-08 23:14:09 -0500

    Fixed the dependencies to work on Pycharm

--- rapid_response_kit/utils/config.py
+++ rapid_response_kit/utils/config.py
@@ -1,11 +1,4 @@
-SECRET_KEY = ''
-TWILIO_ACCOUNT_SID = ''
-TWILIO_AUTH_TOKEN = ''
-
-try: 
-   import local_config
-except:
-  import os
-  SECRET_KEY = os.environ['SECRET_KEY']
-  TWILIO_ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
-  TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
+# Configuration Auto-generated during installation
+SECRET_KEY = '3\xcc\xa4t?9\xc7\x97\xc7\x9b?\xc3\x06\x8a\xbc\x94_\xd17\xaf'
+TWILIO_ACCOUNT_SID = 'ACe1a8ff61599a76eb7a285fc871219d58'
+TWILIO_AUTH_TOKEN = 'e9ef5b1bb241db2822f8b060788ad1da'
